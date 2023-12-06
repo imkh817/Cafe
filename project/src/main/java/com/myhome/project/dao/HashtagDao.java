ecc443a73f30b7bcb51e3824d3ef406823d356ed
jiyeon1227 <wldus991227@naver.com>
2023-12-06 17:50:06 +0900

    2023_12_06 - 목록 페이지 구현 - 상세 페이지 구현     - 상세페이지에서 지도, 리뷰부분 구현

--- project/src/main/java/com/myhome/project/dao/HashtagDao.java
+++ project/src/main/java/com/myhome/project/dao/HashtagDao.java
@@ -1,8 +1,13 @@
 package com.myhome.project.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.myhome.project.model.Hashtag;
 
 @Mapper
 public interface HashtagDao {
 
+	List<Hashtag> gethashtag();
 }
--- project/src/main/java/com/myhome/project/service/ReviewServiceImpl.java
+++ project/src/main/java/com/myhome/project/service/ReviewServiceImpl.java
@@ -29,5 +29,28 @@ public class ReviewServiceImpl implements ReviewService{
 	public int deleteReview(String review_no) {
 		return dao.deleteReview(review_no);
 	}
+	
+	// 리뷰 작성
+	@Override
+	public int review_insert(Review review) {
+		return dao.review_insert(review);
+	}
+
+	@Override
+	public List<Map<String, Object>> review_list(Review review) {
+		return dao.review_list(review);
+	}
+
+	@Override
+	public int getTotal(Review review) {
+		return dao.review_total(review);
+	}
+
+	// 카페 별점 평균값 구해오기
+	@Override
+	public double avg_star(int cafe_no) {
+		return dao.avg_star(cafe_no);
+
+	}
 
 }
